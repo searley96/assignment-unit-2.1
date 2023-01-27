fd0ed773bfde46268d8255d65138c8350ef9c382
Siv Earley <searley96@gmail.com>
2023-01-27 14:02:50 -0600

    completed 17 and 18 stretch probs and asked q in html

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -64,6 +64,8 @@ if (pets < allowedPets) {
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -71,4 +73,23 @@ if (pets < allowedPets) {
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+luckyNumber = 14;
+switch(luckyNumber) {
+    case 1:
+        console.log("First is the worst");
+        break;
+        case 2:
+            console.log("Second is the best");
+            break;
+            case 3:
+                console.log("Third is the one with the polka dot dress");
+                break;
+                default:
+                    console.log("Luck is what happens when preparation meets opportunity");
+                    break;
+};
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+
+message = adventurous ? "Adventures are great!" : "How about we stay home?";
+console.log(message);
